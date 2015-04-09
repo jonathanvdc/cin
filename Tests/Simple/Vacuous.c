e6b3787d7e7323b70b9329681b1eb829e2f92808
jonathanvdc <jonathan.vdc@outlook.com>
2015-04-09 19:02:43 +0200

    Fixed Vacuous.c test
    This test should represent a valid (albeit UB) program.

--- Tests/Simple/Vacuous.c
+++ Tests/Simple/Vacuous.c
@@ -3,9 +3,11 @@ struct A { int x; };
 
 double Test(void)
 {
-    struct A;
-    struct B { struct A y; };
-    struct A { double x; };
+    struct A; // This vacuous variable declaration will get special treatment:
+              // it will declare a *new* 'struct A' in this scope, for usage in
+              // 'struct B'.
+    struct B { struct A *y; };
+    struct A { double x; struct B *y; };
     struct B z;
-    return z.y.x;
+    return z.y->x;
 }
