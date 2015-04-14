09f4c23b0bcea7f3e63a6e59fcf623e95e559a93
jonathanvdc <jonathan.vdc@outlook.com>
2015-04-14 11:51:42 +0200

    cin now pays attention to variable redefinitions

--- Tests/Wrong/Redefinition.c
+++ Tests/Wrong/Redefinition.c
@@ -3,3 +3,18 @@ struct Type { double y; } b;
 
 typedef struct Type A;
 typedef int A;
+
+int Shadowing(int x)
+{
+    int val;
+    int val;
+    if (x > 0)
+    {
+        int val = 3;
+    }
+    else
+    {
+        val = 2;
+    }
+    return val;
+}
